92755370702be48c3f48c952b63f8b305221911d
adika16k <adilhankenzaliev@gmail.com>
2023-04-14 18:05:16 +0600

    1

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,8 +1,17 @@
 public class MyArrayList {
-    private Object[] array;
+    private int[] array;
     private int size = 0;
 
     public void add(int element){
+        if (size == array.length){
 
+        }
+    }
+    public void changeSize(){
+        int[] newArray = new int[array.length * 2];
+        for (int i=0; i< array.length; i++){
+            newArray[i] = array[i];
+        }
+        array = newArray;
     }
 }
