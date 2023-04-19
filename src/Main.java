ddccec660cc8223618f9f5283f2f749b05f14f1d
adika16k <adilhankenzaliev@gmail.com>
2023-04-19 22:21:42 +0600

    1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        MyArrayList<Integer> arrayList = new MyArrayList();
-        arrayList.add(56);
+        MyArrayList<Integer> arrayList = new MyArrayList(); // by <Integer> we give data type for elements in array
+        arrayList.add(56); // adding elements to array
         arrayList.add(1);
         arrayList.add(41);
         arrayList.add(85);
         arrayList.add(8);
 
         System.out.println("After sorting:");
-        arrayList.sort();
-        arrayList.displayElements();
+        arrayList.sort(); // sorting an array
+        arrayList.displayElements(); // print elements of array to console
         System.out.println();
 
         System.out.println("Before adding:");
@@ -17,12 +17,12 @@ public class Main {
         System.out.println();
 
         System.out.println("After adding:");
-        arrayList.add(4);
+        arrayList.add(4); // add element 4 to last for array
         arrayList.displayElements();
         System.out.println();
 
         System.out.println("After removing fourth element:");
-        arrayList.remove(4);
+        arrayList.remove(4); //remove element from index 4 of array
         arrayList.displayElements();
         System.out.println();
 
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,7 +1,7 @@
 
 public class MyArrayList<T> implements Mylist1<T> {
-    private Object[] array;
-    private int size;
+    private Object[] array; // Object it is when elements can take all reference types like String, Integer and etc
+    private int size; // size its length of array
 
     MyArrayList(){
         this.array = new Object[5];
@@ -9,17 +9,17 @@ public class MyArrayList<T> implements Mylist1<T> {
     }
     @Override
     public void add(T element) {
-        if (size == array.length){
+        if (size == array.length){ // it checks if array is full by comparing its size and length
             changeSize();
         }
-        array[size++] = element;
+        array[size++] = element;  // if true size increased by 1 and after that we can add an element
     }
 
     @Override
     public void changeSize() {
-        Object[] newArray = new Object[array.length * 2];
+        Object[] newArray = new Object[array.length * 2]; // creating new array which length will be more twice than first array
         for (int i=0; i< array.length; i++){
-            newArray[i] = array[i];
+            newArray[i] = array[i]; // giving new array elements of first array
         }
         array = newArray;
     }
@@ -27,24 +27,24 @@ public class MyArrayList<T> implements Mylist1<T> {
     @Override
     public void displayElements() {
         for (int i = 0; i < size; i++) {
-            System.out.print(array[i] + " ");
+            System.out.print(array[i] + " "); // to write console all elements of array
         }
     }
 
     @Override
     public T get(int index) {
-        return (T) array[index];
+        return (T) array[index]; // get inputed index of array
     }
 
     @Override
     public void clear() {
-        this.array = (T[]) new Object[5];
+        this.array = (T[]) new Object[5]; // it's like to update the array for zero
         this.size = 0;
     }
 
     @Override
     public void remove(int index) {
-        for (int i = index + 1; i < size; i++) {
+        for (int i = index + 1; i < size; i++) { // remove given index of element
             array[i - 1] = array[i];
         }
         size--;
@@ -57,8 +57,8 @@ public class MyArrayList<T> implements Mylist1<T> {
 
     @Override
     public void sort() {
-        int temp = 0;
-        for (int i=0;i<array.length;i++){
+        int temp = 0; // it's bubble sort of array
+        for (int i=0;i<array.length;i++){  // bubble sort is to compare all elements of array by two elements
             for(int j=1;j< array.length-i;j++){
                 if (array[j-1] != null && array[j] != null && (int) array[j-1] > (int) array[j]){
                     temp = (int) array[j-1];
@@ -71,13 +71,13 @@ public class MyArrayList<T> implements Mylist1<T> {
 
     @Override
     public void removeLast() {
-        size--;
+        size--; // to remove last element of array we can just decrease size for by one
     }
 
     @Override
     public void removeFirst() {
         for (int i=0; i<size-1;i++){
-            array[i] = array[i+1];
+            array[i] = array[i+1]; // it's method to remove first element of array
         }
         size--;
     }
